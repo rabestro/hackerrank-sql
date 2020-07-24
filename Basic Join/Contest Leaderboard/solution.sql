5d1be611abdc055d224b4fe0e79231e19229ae3b
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-07-24 15:59:10 +0300

    Update solution.sql

--- Basic Join/Contest Leaderboard/solution.sql
+++ Basic Join/Contest Leaderboard/solution.sql
@@ -1,10 +1,10 @@
-SELECT h.hacker_id, h.name, SUM(max_score) AS total_score
+SELECT hackers.hacker_id, name, SUM(max_score) AS total_score
 FROM (
     SELECT hacker_id, challenge_id, MAX(score) AS max_score
     FROM submissions
     GROUP BY hacker_id, challenge_id
-) challenges, hackers h
-WHERE challenges.hacker_id = h.hacker_id (+)
-GROUP BY h.hacker_id, h.name
+) challenges, hackers
+WHERE challenges.hacker_id = hackers.hacker_id (+)
+GROUP BY hackers.hacker_id, name
 HAVING SUM(max_score) > 0
-ORDER BY total_score DESC, h.hacker_id ASC;
+ORDER BY total_score DESC, hacker_id ASC;
