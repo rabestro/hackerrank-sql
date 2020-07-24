6fe44dee16a74911636bedde3426876b9341aa59
Jegors Čemisovs <52966251+rabestro@users.noreply.github.com>
2020-07-24 17:47:13 +0300

    Update solution.sql

--- Basic Join/Challenges/solution.sql
+++ Basic Join/Challenges/solution.sql
@@ -1,14 +1,18 @@
-SELECT hacker_id, name, COUNT(challenge_id) 
-FROM Hackers
-NATURAL JOIN Challenges
-GROUP BY hacker_id, name
-HAVING COUNT(challenge_id) NOT IN (
-    SELECT cnt FROM (
+SELECT hacker_id, name, count_ch 
+FROM (
+    SELECT hacker_id, name, COUNT(challenge_id) AS count_ch 
+    FROM Hackers
+    NATURAL JOIN Challenges
+    GROUP BY hacker_id, name 
+)  
+WHERE count_ch NOT IN (
+    SELECT cnt 
+    FROM (
         SELECT hacker_id, COUNT(challenge_id) AS cnt
         FROM Challenges
         GROUP BY hacker_id) 
+    WHERE cnt < (SELECT MAX(COUNT(*)) FROM Challenges GROUP BY hacker_id)
     GROUP BY cnt
-    HAVING COUNT(cnt) > 1
-    ORDER BY cnt DESC
-    OFFSET 1)
-ORDER BY challenges DESC, hacker_id;
+    HAVING COUNT(hacker_id) > 1 
+)
+ORDER BY count_ch DESC, hacker_id;
