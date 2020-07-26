c2d8979eb201a38c5dc9cdcb725ac8d52fdb213c
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-26 20:51:02 +0300

    Added solution for problem "New Company"

--- Advanced Select/New Companies/solution.sql
+++ Advanced Select/New Companies/solution.sql
@@ -1,23 +1,29 @@
-select
+select distinct
     company_code as "Company code",
     founder as "Company founder",
     (
-        select count(lead_manager_code)
+        select count(distinct lead_manager_code)
         from Lead_Manager
         where company_code = Company.company_code
     )
     as "Lead managers",
     (
-        select count(senior_manager_code)
+        select count(distinct senior_manager_code)
         from Senior_Manager
         where company_code = Company.company_code
     )
     as "Senior Managers",
     (
-        select count(manager_code)
+        select count(distinct manager_code)
         from Manager
         where company_code = Company.company_code
     )
-    as "Managers"
+    as "Managers",
+    (
+        select count(distinct employee_code)
+        from Employee
+        where company_code = Company.company_code
+    )
+    as "Employees"
 from Company
 order by company_code;
