3a8dacd975e2177140060b23ccf660da1e3fa206
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-07-27 19:25:02 +0300

    Fixed a bug in solution for "Top Competitors",

--- Basic Join/Top Competitors/solution.sql
+++ Basic Join/Top Competitors/solution.sql
@@ -6,8 +6,8 @@ on Submissions.hacker_id = Hackers.hacker_id
 where score = (
     select score
     from Difficulty
-    where difficult_level = Challenges.difficulty_level
+    where difficulty_level = Challenges.difficulty_level
 )
-group by Submissions.hacker_id
+group by Submissions.hacker_id, name
 having count(Submissions.challenge_id) > 1
 order by count(Submissions.challenge_id) desc, Submissions.hacker_id;
