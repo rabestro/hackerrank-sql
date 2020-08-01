cef90e9decf2efa48755b39aedb9cefdd6afd648
Jegors Čemisovs <yegor.chemisov@gmail.com>
2020-08-01 20:33:51 +0300

    Solved

--- Advanced Join/Symmetric Pairs/solution.sql
+++ Advanced Join/Symmetric Pairs/solution.sql
@@ -1,6 +1,13 @@
+select x, y
+from Functions
+where x = y
+group by x, y
+having count(*) > 1
+union all
 select distinct a.x, a.y
 from Functions a
-join Functions b
-    on a.x = b.y and a.y = b.x
-where a.x <= a.y
-order by a.x;
+         join Functions b
+              on a.x = b.y and a.y = b.x
+where a.x < a.y
+order by x;
+
